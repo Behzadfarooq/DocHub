798341c0d2daeb2ad90bdd05550e8ad2469ad8f3
Behzad Farooq <behzadsbz@gmail.com>
2023-12-09 00:59:24 +0530

    COMMIT

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCVARl-5I3jl2LKnb0gexESrZi1gJv2tAI",
+  apiKey: process.env.API_KEY,
   authDomain: "document-editor-e182f.firebaseapp.com",
   projectId: "document-editor-e182f",
   storageBucket: "document-editor-e182f.appspot.com",
   messagingSenderId: "1010115898229",
-  appId: "1:1010115898229:web:b18f2b6059e9821977013c",
+  appId:  process.env.APP_ID,
   measurementId: "G-L57VRFB2X4"
 };
 
